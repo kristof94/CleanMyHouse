6961ab977e7db5e335877e3add68a1820e4e6ba0
kristof ws <kristof.dsantos@gmail.com>
2019-01-19 19:41:19 +0100

    debug

--- components/Order/Address.vue
+++ components/Order/Address.vue
@@ -7,7 +7,16 @@
             <slot name="header">default header</slot>
           </div>
           <div class="modal-body">
-            <slot name="body"/>
+            <slot name="body">
+              <vue-google-autocomplete
+                id="map"
+                ref="address"
+                class="form-control"
+                placeholder="Entrer votre addresse"
+                country="fr"
+                @placechanged="getAddressData"
+              />
+            </slot>
           </div>
           <div class="modal-footer">
             <slot name="footer">
