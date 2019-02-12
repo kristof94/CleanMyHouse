123bf12a23a6611a7a13670e49d1ee306acf4b06
kristof ws <kristof.dsantos@gmail.com>
2019-02-12 16:11:45 +0100

    test

--- postinstall.js
+++ postinstall.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv')
+var exec = require('child_process').exec
+
 dotenv.config()
 
 console.log('run ', process.env.secretession)
-console.log('run ', process.env.npm_run)
-var exec = require('child_process').exec
+console.log('run ', process.env.hoclick)
 exec(process.env.npm_run, function(err, stdout) {
   if (err) throw err
   else console.log(stdout)
